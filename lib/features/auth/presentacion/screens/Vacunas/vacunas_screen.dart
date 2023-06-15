3c072bf8392d820bcded2fe7dea061fb6a7d9537
Huarachi2002 <roberthuarachi27@gmail.com>
2023-06-15 08:00:13 -0400

    esquema de vacunacion

--- lib/features/auth/presentacion/screens/Vacunas/vacunas_screen.dart
+++ lib/features/auth/presentacion/screens/Vacunas/vacunas_screen.dart
@@ -20,6 +20,10 @@ class _VacunasView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Image(
+          image: NetworkImage(
+              'https://avinews.com/wp-content/uploads/2016/09/plan_de_vacunacion_reproductoras.jpg')),
+    );
   }
 }
